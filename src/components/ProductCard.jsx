5f6ba1517c787d40c3cd0cdc8ab530792ceaf570
root <root@DESKTOP-4GUT5LC.localdomain>
2023-06-30 00:12:42 +0530

    Completed products page. Initializing Context API

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,48 @@
+import React, { useContext } from 'react'
+
+// import CartContext from '../CartContext';  
+
+const ProductCard = ({id , title, price, description, category, image, rating}) => {
+
+
+    // const { cartItems, addToCart } = useContext(CartContext)
+
+
+
+    const addItemToCart = () => {
+        const item = {
+            id, category, description, image, title, price, rating, quantity: 1
+        }
+        // addToCart(item)
+    }
+
+
+
+
+    return (
+        <div className='w-[30%] py-5 px-5 bg-slate-100 rounded-xl shadow-xl flex flex-col justify-center items-left'>
+
+            
+
+            <div className=' w-full flex justify-center align-center my-2'>
+                <img src={image} alt={title} className='rounded-lg h-60' />
+            </div>
+
+            <div className=' flex flex-col gap-4 w-full justify-center mt-4 items-left'>
+                <span className='text-lg font-bold leading-6'>{title}</span>
+                <span className='-mt-3 italics text-sm text-slate-600'>{category}</span>
+                <span></span>
+                <span className='font-sans  font-semibold text-lg'>Price: <span className='italic text-green-600'>${price}</span> </span>
+                <span>{description.slice(0,50)}...</span>
+
+            </div>
+
+            <button onClick={addItemToCart} className='w-full py-4 bg-stone-600 mt-10 font-bold text-slate-100 rounded-lg hover:bg-stone-900 active:translate-y-2'>
+                Add to Cart
+            </button>
+            
+        </div>
+    )
+}
+
+export default ProductCard
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useContext, useEffect, useState } from 'react'
+import ProductCard from '../components/ProductCard';
+
 
 const Shop = () => {
-  return (
-    <div>Shop</div>
-  )
+
+    const [products, setProducts] = useState(null);
+ 
+
+
+    useEffect(() => {
+
+        const fetchProducts = async () => {
+            const data = await axios.get('https://fakestoreapi.com/products')
+            setProducts(data.data)
+        }
+
+        fetchProducts();
+
+    }, [])
+
+    return (
+        <div className='flex justify-center align-top px-10 py-10 gap-10 flex-wrap bg-slate-200'>
+            {
+                products && products.map((product) => {
+                    return (
+                        <ProductCard
+                            key={product.id}
+                            category={product.category}
+                            description={product.description}
+                            id={product.id}
+                            image={product.image}
+                            price={product.price}
+                            rating={product.rating}
+                            title={product.title}
+                        />
+                    )
+                })
+            }
+        </div>
+    )
 }
 
 export default Shop
